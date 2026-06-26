5c4f07bf93ea770e07e6b012694799874b217096
codeuv30 <codeuv30@gmail.com>
2026-06-26 11:59:32 +0530

    day - 3

--- Day - 1/src/main.js
+++ Day - 1/src/main.js
@@ -1,18 +1,43 @@
 import "./style.css";
 import gsap from "gsap";
 
-let a = 0;
 
-/* Callback functions */
+let count = 0;
 
-gsap.to(".box", {
-  x: 500,
-  duration: 1.4,
-  delay: 0.6,
-  ease: "power2.inOut",
+const loaderCount = document.querySelector(".loaderCount h1");
 
-  onUpdate: () => {
-    a+=1;
-    console.log('FRAME', a);
-  },
-});
+const interval = setInterval(() => {
+  count++;
+  loaderCount.innerHTML = `${count}%`
+
+  if(count === 100) {
+    clearInterval(interval);
+    landingAnimation();
+  }
+}, 20);
+
+function landingAnimation() {
+  const tl = gsap.timeline();
+
+  tl.to(".loaderCount", {
+    opacity: 0,
+    duration: 1.6,
+    ease: "power3.out"
+  }).to(".loader", {
+    yPercent: -100,
+    duration: 1.2,
+    ease: 'expo.out'
+  }, "-=0.9").from(".background img", {
+    scale: 1.2,
+    duration: 1.3,
+    ease: 'expo.out'
+  }, "-=0.87").from('.heading h1', {
+    yPercent: 100,
+    duration: 1.2,
+    ease: "expo.out"
+  }, "-=0.8").from('.subheading h2', {
+    yPercent: 100,
+    duration: 1.2,
+    ease: "expo.out"
+  }, "-=0.77");
+}
